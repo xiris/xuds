4d67ebc23a524624782bd7017fe8b2edf0b0634c
Christopher Silva <chris@planner.net.br>
2015-01-10 22:11:12 -0200

    Removing comment

--- src/Xiris/Xuds/DatabaseUpdate.php
+++ src/Xiris/Xuds/DatabaseUpdate.php
@@ -2,8 +2,6 @@
 /*
  * This file is part of the Xuds package, an Xiris project.
  *
- * (c) 2014 OpenSky Project Inc
- *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
